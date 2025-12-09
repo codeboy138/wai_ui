8ff5dd0c43ab0054e28d4bc2c811fd1e9cb5ca63
codeboy138 <cwicwi2016@gmail.com>
2025-12-09 21:30:04 +0900

    [Auto] Update js/components/RulerLine.js

--- frontend/js/components/RulerLine.js
+++ frontend/js/components/RulerLine.js
@@ -1,41 +1,45 @@
-/**
- * [DATA-DEV: RulerLine]
- * - 역할: 캔버스 눈금자 (가로/세로)
- * - 고유ID: ruler-line
- * - 기능: 캔버스 가이드 눈금 표시
- * - 로직: direction prop에 따라 가로/세로 렌더링
- * - 데이터: direction (horizontal | vertical)
- * - 경로: frontend/js/components/RulerLine.js
- * - 명령: 없음 (표시 전용)
- */
-
-export default {
-  name: 'RulerLine',
-  props: {
-    direction: {
-      type: String,
-      default: 'horizontal',
-      validator: (value) => ['horizontal', 'vertical'].includes(value)
+// Ruler Line Component
+const RulerLine = {
+    props: ['orientation', 'maxSize', 'scale'],
+    template: `
+        <div class="absolute inset-0 overflow-hidden" :style="wrapperStyle">
+            <template v-if="orientation === 'h'">
+                <div v-for="i in majorTicks" :key="i" :style="horizontalTickStyle(i)" class="absolute top-0 text-xxs text-text-sub font-mono pl-1 border-l border-ui-border h-full">
+                    {{ i }}
+                </div>
+            </template>
+            <template v-else>
+                <div v-for="i in majorTicks" :key="i" :style="verticalTickStyle(i)" class="absolute left-0 text-xxs text-text-sub font-mono pt-px border-t border-ui-border w-full">
+                    {{ i }}
+                </div>
+            </template>
+        </div>
+    `,
+    computed: {
+        majorTicks() {
+            const step = 100; // Major tick every 100px in 4K space
+            const ticks = [];
+            const maxScaledSize = this.maxSize * this.scale;
+            
+            for (let i = step; i < this.maxSize; i += step) {
+                if (i * this.scale < (this.orientation === 'h' ? 3840 : 2160)) {
+                    ticks.push(i);
+                }
+            }
+            return ticks;
+        },
+        wrapperStyle() {
+            return this.orientation === 'h' ? 
+                { width: this.maxSize * this.scale + 'px', height: '100%', transformOrigin: 'top left', transform: `scaleX(${1/this.scale})` } : 
+                { width: '100%', height: this.maxSize * this.scale + 'px', transformOrigin: 'top left', transform: `scaleY(${1/this.scale})` };
+        }
+    },
+    methods: {
+        horizontalTickStyle(val) {
+            return { left: val + 'px' };
+        },
+        verticalTickStyle(val) {
+            return { top: val + 'px' };
+        }
     }
-  },
-  template: '<div :id="rulerId" :class="rulerClass" :data-action="dataAction" :title="title"></div>',
-  
-  computed: {
-    rulerId() {
-      return 'ruler-line-' + this.direction;
-    },
-    rulerClass() {
-      return 'c-ruler-line c-ruler-line--' + this.direction;
-    },
-    dataAction() {
-      return 'js:ruler-' + this.direction;
-    },
-    title() {
-      return this.direction === 'horizontal' ? '가로 눈금자' : '세로 눈금자';
-    }
-  },
-  
-  mounted() {
-    console.log('[RulerLine] 마운트:', this.direction);
-  }
 };
