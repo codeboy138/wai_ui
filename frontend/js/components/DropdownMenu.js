e9e6927bc443a03b5487c03444bc15fa2ca916d1
codeboy138 <cwicwi2016@gmail.com>
2025-12-09 19:52:56 +0900

    [Auto] Update js/components/DropdownMenu.js

--- frontend/js/components/DropdownMenu.js
+++ frontend/js/components/DropdownMenu.js
@@ -1,74 +1,75 @@
 /**
- * ==========================================
- * DropdownMenu.js - 드롭다운 메뉴 컴포넌트
- * 
- * 역할: 재사용 가능한 드롭다운 UI
- * 경로: frontend/js/components/DropdownMenu.js
- * ==========================================
+ * [DATA-DEV: DropdownMenu]
+ * - 역할: 재사용 가능한 드롭다운 메뉴 UI
+ * - 고유ID: dropdown-menu
+ * - 기능: 메뉴 토글, 항목 선택
+ * - 로직: isOpen 상태 토글, 외부 클릭 시 닫기
+ * - 데이터: items (배열), isOpen (boolean)
+ * - 경로: frontend/js/components/DropdownMenu.js
+ * - 명령: 없음 (UI 컴포넌트)
  */
 
 export default {
-    name: 'DropdownMenu',
-    
-    props: {
-        currentValue: {
-            type: String,
-            required: true
-        },
-        items: {
-            type: Array,
-            required: true
-        },
-        id: {
-            type: String,
-            required: true
-        }
+  name: 'DropdownMenu',
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    },
+    buttonText: {
+      type: String,
+      default: 'Menu'
+    }
+  },
+  data() {
+    return {
+      isOpen: false
+    };
+  },
+  template: '<div class="c-dropdown-menu" data-action="js:dropdownMenu" title="드롭다운 메뉴">' +
+            '<button class="c-dropdown-menu__button" @click="toggle" :title="buttonText">{{ buttonText }}</button>' +
+            '<div v-if="isOpen" class="c-dropdown-menu__list">' +
+            '<div v-for="(item, index) in items" :key="index" class="c-dropdown-menu__item" @click="selectItem(item)" :title="item.label">{{ item.label }}</div>' +
+            '</div>' +
+            '</div>',
+  
+  methods: {
+    toggle() {
+      this.isOpen = !this.isOpen;
+      console.log('[DropdownMenu] 토글:', this.isOpen);
     },
     
-    data() {
-        return {
-            isOpen: false
-        };
+    selectItem(item) {
+      console.log('[DropdownMenu] 선택:', item);
+      this.$emit('select', item);
+      this.isOpen = false;
+    }
+  },
+  
+  mounted() {
+    document.addEventListener('click', this.handleOutsideClick);
+  },
+  
+  beforeUnmount() {
+    document.removeEventListener('click', this.handleOutsideClick);
+  },
+  
+  methods: {
+    handleOutsideClick(e) {
+      if (!this.$el.contains(e.target)) {
+        this.isOpen = false;
+      }
     },
     
-    methods: {
-        toggleDropdown() {
-            this.isOpen = !this.isOpen;
-        },
-        
-        closeOnOutsideClick(e) {
-            if (!this.$el.contains(e.target)) {
-                this.isOpen = false;
-            }
-        }
+    toggle() {
+      this.isOpen = !this.isOpen;
+      console.log('[DropdownMenu] 토글:', this.isOpen);
     },
     
-    mounted() {
-        document.addEventListener('click', this.closeOnOutsideClick);
-    },
-    
-    beforeUnmount() {
-        document.removeEventListener('click', this.closeOnOutsideClick);
-    },
-    
-    template: \`
-        <div :id="id" 
-             class="c-dropdown c-dropdown--compact" 
-             :class="{ 'c-dropdown--open': isOpen }" 
-             @click="toggleDropdown" 
-             @click.stop
-             :title="'선택: ' + currentValue">
-            <span class="c-dropdown__value">{{ currentValue }}</span>
-            <i class="c-dropdown__icon fa-solid fa-caret-down"></i>
-            <div class="c-dropdown__menu">
-                <div class="c-dropdown__item" 
-                     v-for="item in items" 
-                     :key="item" 
-                     :title="'선택: ' + item"
-                     @click="$emit('select', item); isOpen = false">
-                    {{ item }}
-                </div>
-            </div>
-        </div>
-    \`
+    selectItem(item) {
+      console.log('[DropdownMenu] 선택:', item);
+      this.$emit('select', item);
+      this.isOpen = false;
+    }
+  }
 };
